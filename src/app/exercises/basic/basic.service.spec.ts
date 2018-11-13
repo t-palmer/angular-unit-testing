a7826abc5b328fa0ee6fb0475c08c0837ef83a43
Todd Palmer <tpalmer@us.fujitsu.com>
2018-11-12 19:13:43 -0500

    Before Testing a Service

--- src/app/exercises/basic/basic.service.spec.ts
+++ src/app/exercises/basic/basic.service.spec.ts
@@ -1,12 +1,18 @@
 import { TestBed } from '@angular/core/testing';
-
 import { BasicService } from './basic.service';
 
-describe('BasicService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+describe('BasicService ...', () => {
 
-  it('should be created', () => {
-    const service: BasicService = TestBed.get(BasicService);
+  let service: BasicService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+    });
+
+    service = TestBed.get(BasicService);
+  });
+
+  it('- should be created', () => {
     expect(service).toBeTruthy();
   });
 });
