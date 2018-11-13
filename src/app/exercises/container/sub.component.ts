e411ab45b202880fd8989e9549d4de34fce2b222
Todd Palmer <tpalmer@us.fujitsu.com>
2018-11-12 19:57:47 -0500

    Sub Component

---
+++ src/app/exercises/container/sub.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sub',
+  templateUrl: './sub.component.html',
+  styleUrls: ['./sub.component.css']
+})
+export class SubComponent {
+
+}
--- src/app/exercises/exercises.module.ts
+++ src/app/exercises/exercises.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DomComponent } from './dom/dom.component';
+import { ContainerComponent } from './container/container.component';
+import { SubComponent } from './container/sub.component';
 
 @NgModule({
   declarations: [
+    ContainerComponent,
+    SubComponent,
     DomComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
+    ContainerComponent,
+    SubComponent,
     DomComponent
   ]
 })
