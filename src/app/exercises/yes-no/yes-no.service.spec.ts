021c9ef42adb2561d2bd3c7f2fd9eca7759aead3
Todd Palmer <tpalmer@us.fujitsu.com>
2018-11-12 19:29:24 -0500

    After Testing a Service

--- src/app/exercises/yes-no/yes-no.service.spec.ts
+++ src/app/exercises/yes-no/yes-no.service.spec.ts
@@ -1,12 +1,43 @@
 import { TestBed } from '@angular/core/testing';
-
 import { YesNoService } from './yes-no.service';
 
-describe('YesNoService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+describe('YesNoService ...', () => {
 
-  it('should be created', () => {
-    const service: YesNoService = TestBed.get(YesNoService);
+  let service: YesNoService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+    });
+
+    service = TestBed.get(YesNoService);
+  });
+
+  it('- should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  // Add a describe to test the getString function
+  describe('getString ...', () => {
+    it(`- should return 'Yes' when passed 'true'`, () => {
+      // Arrange
+      let result: string;
+
+      // Act
+      result = service.getString(true);
+
+      // Assert
+      expect(result).toEqual('Yes');
+    });
+
+    it(`- should return 'No' when passed 'false'`, () => {
+      // Arrange
+      let result: string;
+
+      // Act
+      result = service.getString(false);
+
+      // Assert
+      expect(result).toEqual('No');
+    });
   });
 });
